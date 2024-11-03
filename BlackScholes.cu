15647ac516ddf146f458e3d23b076d72522ab98a
Vmpletsos <msa17020@gmail.com>
2024-11-03 23:48:52 +0200

    Mistake in number

--- BlackScholes.cu
+++ BlackScholes.cu
@@ -107,7 +107,7 @@ int main(int argc, char **argv)
     for (i = 0; i < REPEAT_ITERATIONS_EXPERIMENT; i++)
     {
         // because we have 858 options, we need to launch 13 blocks of 66 threads
-        BlackScholesGPU<<<1024, 66>>>(
+        BlackScholesGPU<<<13, 66>>>(
             (int1 *)d_OptionTypes,
             (float1 *)d_StockPrice,
             (float1 *)d_OptionStrike,
